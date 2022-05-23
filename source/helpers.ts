0adc721cd421f8f38b6c0bd4a7c42d890cc6e057
Jamie Cope <jamie@gcope.com>
2022-05-23 15:06:36 -0400

    Fixes to support Three.js 139 update

--- source/helpers.ts
+++ source/helpers.ts
@@ -44,7 +44,7 @@ export function quaternionToDegrees(quaternion: Quaternion, order: string, rotat
     const result = rotation || [ 0, 0, 0 ];
 
     _euler.setFromQuaternion(quaternion, order);
-    _euler.toVector3(_vec3);
+    _vec3.setFromEuler(_euler);
     _vec3.multiplyScalar(MathUtils.RAD2DEG).toArray(result);
 
     return result;
