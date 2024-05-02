08b584ce60deb267ec9a35d128f09ac8c9986858
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-05-02 09:24:53 +0200

    use pivot point in zoomExtents

--- source/CameraController.ts
+++ source/CameraController.ts
@@ -194,12 +194,16 @@ export default class CameraController implements IManip
 
         // rotate box to camera space
         _vec3a.copy(this.orbit).multiplyScalar(math.DEG2RAD);
+        _quat.setFromEuler(_eua.setFromVector3(_vec3a));
         _vec3b.setScalar(0);
-        threeMath.composeOrbitMatrix(_vec3a, _vec3b, _mat4);
+        _vec3c.setScalar(1);
+        //Ignore the pivot point for now
+        _mat4.compose(_vec3b, _quat, _vec3c);
 
         _box3.copy(box).applyMatrix4(_mat4.transpose());
         _box3.getSize(_vec3a);
         _box3.getCenter(_vec3b);
+        _vec3b.sub(this.pivot);
 
         offset.x = _vec3b.x;
         offset.y = _vec3b.y;
